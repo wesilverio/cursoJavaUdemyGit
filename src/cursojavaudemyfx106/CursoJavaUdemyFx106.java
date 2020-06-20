c9ddcf832fba63f4602c31c5ed2e24f88d7a5884
wesilverio <silveiro.wes91@gmail.com>
2020-06-19 22:58:51 -0300

    Acrescentada a Msg de Boa Tarde

--- src/cursojavaudemyfx106/CursoJavaUdemyFx106.java
+++ src/cursojavaudemyfx106/CursoJavaUdemyFx106.java
@@ -6,6 +6,7 @@ public class CursoJavaUdemyFx106 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("BOM DIA!");
+        System.out.println("BOM TARDE!");
     }
     
 }
